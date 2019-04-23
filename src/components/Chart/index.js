de4f8d7c4e9401a0336013d35f8bc818e2a5852d
songjun <songjun@broada.com>
2019-04-23 09:46:20 +0800

    update:组件拖拽计算边界

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -2,16 +2,22 @@ import React, { Component } from "react";
 import ChartCreator from "modules/charts";
 import PropTypes from "prop-types";
 import Event from "utils/event";
+
 class Chart extends Component {
   static propTypes = {
     widget: PropTypes.object
   };
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+        left: props.widget.left,
+        top: props.widget.top
+    }
   }
 
   componentDidMount() {
     this.createChart();
+
   }
 
   // 创建图表实例
@@ -21,6 +27,7 @@ class Chart extends Component {
       this.chart = chart;
       this.chart.render();
       draggle.init();
+      this.calculateReact()
       Event.on("widgetResize", size => {
         this.chart.resize();
       });
@@ -31,10 +38,28 @@ class Chart extends Component {
     });
   }
 
+    // 计算边界条件（避免超出下和右边界）
+  calculateReact() {
+    const { left, top, width, height } = this.props.widget;
+    const wrapWidth = draggle.$container.css("width")
+    const wrapHeight = draggle.$container.css("height")
+    if (parseInt(width + left) > parseInt(wrapWidth)) {
+      this.setState({
+        left: parseInt(wrapWidth) - width
+      })
+    }
+    if (parseInt(height + top) > parseInt(wrapHeight)) {
+      this.setState({
+        top: parseInt(wrapHeight) - height
+      })
+    }
+  }
+
   render() {
-    const { id, left, top, width, height } = this.props.widget;
+    const { id, width, height } = this.props.widget;
+    const { left, top, } = this.state
     return (
-      <div className="dragger"  style={{ left, top, width, height }}>
+      <div className="dragger" style={{ left, top, width, height }}>
         <div className="chart" ref="chart" id={id} />
         <span className="resize-handle" />
       </div>
